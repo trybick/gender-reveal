b7ab105d055169b04e432d887d4e96ac037fd2bb
Tim Rybicki <39889198+trybick@users.noreply.github.com>
2020-10-21 08:43:46 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import * as graphql from "../mlp/tmp.js";
+import karate from "./karate.gif";
+import "./styles.css";
+
+export default function App() {
+  const [isRevealed, setIsRevealed] = React.useState(false);
+  const [loading, setIsLoading] = React.useState(false);
+  const [counter, setCounter] = React.useState(graphql.s);
+  const [isFixed, setIsFixed] = React.useState(false);
+
+  React.useEffect(() => {
+    if (loading === true) {
+      const timer =
+        counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+      return () => clearInterval(timer);
+    }
+  }, [counter, loading]);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsRevealed(true);
+    }, graphql.t);
+  };
+
+  return (
+    <div className="wrapper">
+      {!isRevealed && !loading && (
+        <>
+          <div className="intro">Click the button to reveal the gender!</div>
+          <button className="reveal-btn" onClick={handleClick}>
+            Reveal{" "}
+            <span aria-label="baby" role="img">
+              👶
+            </span>
+          </button>
+        </>
+      )}
+
+      {loading && !isRevealed && (
+        <div className="loading">
+          (building suspense)
+          <div>Countdown: {counter}</div>
+          <img className="karate" src={karate} alt="loading..." />
+        </div>
+      )}
+
+      {isRevealed && !isFixed && (
+        <div className="revealed">
+          {graphql.r}
+          <span>{graphql.u}</span>
+
+          <div className="for-real">
+            Oops looks like a bug{" "}
+            <span aria-label="facepalm" role="img">
+              🤦‍♂️
+            </span>
+            <br />
+            Click again -
+            <br />
+            <button className="reveal-btn" onClick={() => setIsFixed(true)}>
+              For real though {" "}
+            </button>
+          </div>
+        </div>
+      )}
+
+      {isFixed && (
+        <div className="final">
+          {graphql.x}
+          <br />
+          {graphql.y}
+        </div>
+      )}
+    </div>
+  );
+}
